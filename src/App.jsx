85a23cfbd44a9ac4b4c8899a03a105efea0de000
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-10-30 14:07:06 +0200

    Update App.jsx add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.styled';
 import { GlobalStyled } from './components/GlobalStyle/GlobalStyle.styled';
+import { Header } from './components/Header/Header';
 
 export const App = () => {
-  
+
   return (
     <GlobalStyled>
-
+      <Header/>
     </GlobalStyled>
   );
 }
