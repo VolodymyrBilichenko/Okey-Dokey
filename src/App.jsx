037fc6e1b7b87c62b312379be536ea42593c3568
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-01 16:43:50 +0200

    Create IconSprite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.styled';
+import {Header} from "./components/Header/Header";
+import {IconSprite} from "./components/IconSprite";
 
 export const App = () => {
 
     return (
         <>
-
+            <IconSprite/>
+            <Header/>
         </>
     );
 }
