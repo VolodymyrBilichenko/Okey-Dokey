f94f0f563228356133faef317440b329fbe6b24f
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-01 17:24:44 +0200

    Create MainTarrifs component

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {MainHero} from "./components/MainHero/MainHero";
+import {MainTariffs} from "./components/MainTariffs/MainTariffs";
 
 export const Main = () => {
   return (
-      <MainHero/>
+      <>
+        <MainHero/>
+        <MainTariffs/>
+      </>
   );
 }
---
+++ src/pages/Main/components/MainTariffs/MainTariffs.jsx
@@ -0,0 +1,152 @@
+import React from 'react';
+
+import tariffsCardPattern from '../../../../assets/img/index/tariffs/tariffs-card-pattern.svg'
+import cardDecorImage from '../../../../assets/img/index/tariffs/tariffs-card-image.svg'
+export const MainTariffs = () => {
+  return (
+      <section className="tariffs container" id="tariffs">
+          <h2 className="tariffs__title section-title _large">
+              Тарифы
+          </h2>
+          <ul className="tariffs__list">
+              <li className="tariffs__card">
+                  <div className="tariffs__card--body">
+                      <div className="tariffs__card--background">
+                          <div className="tariffs__card--decor"></div>
+                          <div className="tariffs__card--pattern">
+                              <img src={tariffsCardPattern} width="0" height="0" loading="lazy" alt=""/>
+                          </div>
+                      </div>
+                      <div className="tariffs__card--image">
+                          <img src={cardDecorImage} width="180" height="185" loading="lazy" alt=""/>
+                      </div>
+                      <div className="tariffs__card--content">
+                          <h3>I can sing English </h3>
+                          <p>
+                              дети 3-9 лет 3 500 грн
+                          </p>
+                      </div>
+                      <a href="#" className="tariffs__card--btn btn _min-3">
+                          Ознакомиться
+                      </a>
+                  </div>
+              </li>
+
+              <li className="tariffs__card">
+                  <div className="tariffs__card--body">
+                      <div className="tariffs__card--background">
+                          <div className="tariffs__card--decor"></div>
+                          <div className="tariffs__card--pattern">
+                              <img src={tariffsCardPattern} width="0" height="0" loading="lazy" alt=""/>
+                          </div>
+                      </div>
+                      <div className="tariffs__card--image">
+                          <img src={cardDecorImage} width="180" height="185" loading="lazy" alt=""/>
+                      </div>
+                      <div className="tariffs__card--content">
+                          <h3>I can sing English </h3>
+                          <p>
+                              дети 3-9 лет 3 500 грн
+                          </p>
+                      </div>
+                      <a href="#" className="tariffs__card--btn btn _min-3">
+                          Ознакомиться
+                      </a>
+                  </div>
+              </li>
+
+              <li className="tariffs__card">
+                  <div className="tariffs__card--body">
+                      <div className="tariffs__card--background">
+                          <div className="tariffs__card--decor"></div>
+                          <div className="tariffs__card--pattern">
+                              <img src={tariffsCardPattern} width="0" height="0" loading="lazy" alt=""/>
+                          </div>
+                      </div>
+                      <div className="tariffs__card--image">
+                          <img src={cardDecorImage} width="180" height="185" loading="lazy" alt=""/>
+                      </div>
+                      <div className="tariffs__card--content">
+                          <h3>I can sing English </h3>
+                          <p>
+                              дети 3-9 лет 3 500 грн
+                          </p>
+                      </div>
+                      <a href="#" className="tariffs__card--btn btn _min-3">
+                          Ознакомиться
+                      </a>
+                  </div>
+              </li>
+
+              <li className="tariffs__card">
+                  <div className="tariffs__card--body">
+                      <div className="tariffs__card--background">
+                          <div className="tariffs__card--decor"></div>
+                          <div className="tariffs__card--pattern">
+                              <img src={tariffsCardPattern} width="0" height="0" loading="lazy" alt=""/>
+                          </div>
+                      </div>
+                      <div className="tariffs__card--image">
+                          <img src={cardDecorImage} width="180" height="185" loading="lazy" alt=""/>
+                      </div>
+                      <div className="tariffs__card--content">
+                          <h3>I can sing English </h3>
+                          <p>
+                              дети 3-9 лет 3 500 грн
+                          </p>
+                      </div>
+                      <a href="#" className="tariffs__card--btn btn _min-3">
+                          Ознакомиться
+                      </a>
+                  </div>
+              </li>
+
+              <li className="tariffs__card">
+                  <div className="tariffs__card--body">
+                      <div className="tariffs__card--background">
+                          <div className="tariffs__card--decor"></div>
+                          <div className="tariffs__card--pattern">
+                              <img src={tariffsCardPattern} width="0" height="0" loading="lazy" alt=""/>
+                          </div>
+                      </div>
+                      <div className="tariffs__card--image">
+                          <img src={cardDecorImage} width="180" height="185" loading="lazy" alt=""/>
+                      </div>
+                      <div className="tariffs__card--content">
+                          <h3>I can sing English </h3>
+                          <p>
+                              дети 3-9 лет 3 500 грн
+                          </p>
+                      </div>
+                      <a href="#" className="tariffs__card--btn btn _min-3">
+                          Ознакомиться
+                      </a>
+                  </div>
+              </li>
+
+              <li className="tariffs__card">
+                  <div className="tariffs__card--body">
+                      <div className="tariffs__card--background">
+                          <div className="tariffs__card--decor"></div>
+                          <div className="tariffs__card--pattern">
+                              <img src={tariffsCardPattern} width="0" height="0" loading="lazy" alt=""/>
+                          </div>
+                      </div>
+                      <div className="tariffs__card--image">
+                          <img src={cardDecorImage} width="180" height="185" loading="lazy" alt=""/>
+                      </div>
+                      <div className="tariffs__card--content">
+                          <h3>I can sing English </h3>
+                          <p>
+                              дети 3-9 лет 3 500 грн
+                          </p>
+                      </div>
+                      <a href="#" className="tariffs__card--btn btn _min-3">
+                          Ознакомиться
+                      </a>
+                  </div>
+              </li>
+          </ul>
+      </section>
+  );
+}
