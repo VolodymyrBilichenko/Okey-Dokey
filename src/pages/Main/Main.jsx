f2a713e314b7b63bf94901cfff1a162f64dfdf9a
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-01 19:36:19 +0200

    Update Main.jsx

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import {MainHero} from "./components/MainHero/MainHero";
 import {MainTariffs} from "./components/MainTariffs/MainTariffs";
+import {MainAboutUs} from "./components/MainAboutUs/MainAboutUs";
+import {MainOurTeachers} from "./components/MainOurTeachers/MainOurTeachers";
+import {MainLeaveApplication} from "./components/MainLeaveApplication/MainLeaveApplication";
+import {MainProgramms} from "./components/MainProgramms/MainProgramms";
+import {MainLevelEnglish} from "./components/MainLevelEnglish/MainLevelEnglish";
 
 export const Main = () => {
-  return (
-      <>
-        <MainHero/>
-        <MainTariffs/>
-      </>
-  );
+    return (
+        <>
+            <MainHero/>
+
+            <MainTariffs/>
+
+            <MainAboutUs/>
+
+            <MainOurTeachers/>
+
+            <MainLeaveApplication/>
+
+            <MainProgramms/>
+
+            <MainLevelEnglish/>
+        </>
+    );
 }
