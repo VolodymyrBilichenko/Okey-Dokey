1ac381137a09a4998a7efbcaf55465159ab81c8a
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-01 12:10:11 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.styled';
-import {Header} from './components/Header/Header';
-import {Globalstyle} from "./components/GlobalStyle/GlobalStyle";
+// import {Header} from './components/Header/Header';
+// import {Globalstyle} from "./components/GlobalStyle/GlobalStyle";
+import {GlobalStyled} from "./components/GlobalStyle/AllStyles/GlobalStyle.styled";
 
 export const App = () => {
 
     return (
         <>
-            <Globalstyle/>
-            <Header/>
+            {/*<Globalstyle/>*/}
+            <GlobalStyled>
+            {/*<Header/>*/}
+
+            </GlobalStyled>
         </>
     );
 }
