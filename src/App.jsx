153a24df5a35e53b1b08910945d446c8f0edaf45
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-06 16:00:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import {useState} from "react";
 import {IconSprite} from "./components/Sprite/IconSprite";
 import {Header} from "./components/Header/Header";
-// import {Main} from "./pages/Main/Main";
 import {Footer} from "./components/Footer/Footer";
 import {routes} from "./functions/routes";
 
@@ -18,8 +17,6 @@ export const App = () => {
             <Routes>
                 {routesList.map(route => <Route key={route.path} element={route.element} path={route.path} />)}
             </Routes>
-
-            {/*<Main/>*/}
 
             <Footer/>
         </>
