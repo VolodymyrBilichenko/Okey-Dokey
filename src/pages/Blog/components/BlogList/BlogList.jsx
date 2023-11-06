da79eb6896a35d2fde09d0afe9f40af6948011a9
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-06 13:44:25 +0200

    Create BlogList components and item

---
+++ src/pages/Blog/components/BlogList/BlogList.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {BlogListItem} from "./components/BlogListItem";
+
+export const BlogList = () => {
+    return (
+        <ul className="blog__list">
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+
+            <BlogListItem/>
+        </ul>
+    );
+}
