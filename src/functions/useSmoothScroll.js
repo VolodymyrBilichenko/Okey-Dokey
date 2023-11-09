c7f9ca57bc7fff481849609dce7c8a081b507943
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-09 13:54:50 +0200

    Create useSmoothScroll.js

---
+++ src/functions/useSmoothScroll.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {useLocation} from "react-router-dom";
+
+export const useSmoothScroll = (hash, pathname) => {
+    useEffect(() => {
+        const headerNavLink = document.querySelector('.header__nav--list a');
+
+        if (headerNavLink && hash) {
+            const targetId = hash.substring(1);
+
+            if (document.querySelector(`#${targetId}`)) {
+                document.querySelector(`#${targetId}`).scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+                window.history.pushState('', '', `#${targetId}`);
+            }
+        }
+    }, [pathname, hash]);
+}
