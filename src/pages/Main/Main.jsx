1126bf920d18ab07c3f83f42dc57372b3e040244
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-09 17:07:45 +0200

    change name files

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,7 +4,7 @@ import {MainTariffs} from "./components/MainTariffs/MainTariffs";
 import {MainAboutUs} from "../../components/MainAboutUs/MainAboutUs";
 import {MainOurTeachers} from "../../components/MainOurTeachers/MainOurTeachers";
 import {MainLeaveApplication} from "../../components/MainLeaveApplication/MainLeaveApplication";
-import {MainProgramms} from "../../components/MainProgramms/MainProgramms";
+import {MainPrograms} from "../../components/MainProgramms/MainPrograms";
 import {MainLevelEnglish} from "./components/MainLevelEnglish/MainLevelEnglish";
 import {MainReviews} from "../../components/MainReviews/MainReviews";
 import {MainFaq} from "./components/MainFaq/MainFaq";
@@ -22,7 +22,7 @@ export const Main = () => {
 
             <MainLeaveApplication/>
 
-            <MainProgramms/>
+            <MainPrograms/>
 
             <MainLevelEnglish/>
 
