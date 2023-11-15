85162b0c389a1bd3819e0e8c9bff832d220acc56
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-11-15 17:51:03 +0200

    add getAllFaq

---
+++ src/api/apiRequests.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import {getApi} from "./getApi";
+
+export async function getAllPrograms() {
+    try{
+        const response = await axios.get(getApi('/api/course/'));
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching all programs:', error);
+        throw error;
+    }
+}
+
+export async function getAllFaq() {
+    try{
+        const response = await axios.get(getApi('/api/base/faq/'));
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching all faq:', error);
+        throw error;
+    }
+}
